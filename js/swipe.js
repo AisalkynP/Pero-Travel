c9e84b8876b971075fd2e91794b9514b7aeea189
KimKarina <kiiim_kk@mail.ru>
2022-03-31 23:23:37 +0600

    two sections, swiper done

---
+++ js/swipe.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".route__swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
