aaaa676a85261ee85318f22b5c276de0678b08e0
KimKarina <kiiim_kk@mail.ru>
2022-04-02 03:25:08 +0600

    done everything except swipers

--- js/swipe.js
+++
@@ -1,8 +0,0 @@
-var swiper = new Swiper(".route__swiper", {
-  slidesPerView: "auto",
-  spaceBetween: 30,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
